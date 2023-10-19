888658bd98a4795c127ee4b2eac699555f1c1304
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-10-19 18:19:14 +0530

    removed unwanted code

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ResourceAndAssignTask.Client;
 using Syncfusion.Blazor;
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmpCf1JpRnxbf1xzZFNMZFpbRHRPMyBoS35RdURjWXped3FVR2lVWEN3");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
