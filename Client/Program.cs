c80f8eb65b55cbda63849e717cc75e55c0e8504a
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-10-19 18:15:53 +0530

    Update the project

---
+++ Client/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ResourceAndAssignTask.Client;
+using Syncfusion.Blazor;
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmpCf1JpRnxbf1xzZFNMZFpbRHRPMyBoS35RdURjWXped3FVR2lVWEN3");
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
+await builder.Build().RunAsync();
